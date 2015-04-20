9c814fb393310ac21d21a1734cb4b17cbab0c793
Tomoka Baba <baba@robox.org>
2015-04-20 18:42:01 +0900

    [test] check to display multi maps.

--- coffee/notusepost-map.coffee
+++ coffee/notusepost-map.coffee
@@ -1,6 +1,20 @@
 initialize = ->
-  jQuery('document').find('[id^=map_canvas]').each (index) ->
-    console.log @, index
+  jQuery(document).find('[id^=map_canvas]').each ->
+    postInfo = window['kodamaps_posts_'+jQuery(@).attr('id').replace('map_canvas_','')]
+    initLat = if postInfo.centerLat? then postInfo.centerLat else 31.773827
+    initLng = if postInfo.centerLng? then postInfo.centerLng else 130.7518837
+    initZoom = if postInfo.zoom? and postInfo.zoom isnt '' then parseInt postInfo.zoom,10 else 10
+
+    opt =
+      zoom: initZoom
+      center: new google.maps.LatLng initLat,initLng
+      mapTypeid: google.maps.MapTypeId.ROADMAP
+
+    map = new google.maps.Map @,opt
+    marker = new google.maps.Marker
+      position: new google.maps.LatLng initLat,initLng
+      map: map
+    map.panTo new google.maps.LatLng initLat,initLng
     return
   return
 
--- js/notusepost-map.js
+++ js/notusepost-map.js
@@ -3,8 +3,23 @@
   var initialize;
 
   initialize = function() {
-    jQuery('document').find('[id^=map_canvas]').each(function(index) {
-      console.log(this, index);
+    jQuery(document).find('[id^=map_canvas]').each(function() {
+      var initLat, initLng, initZoom, map, marker, opt, postInfo;
+      postInfo = window['kodamaps_posts_' + jQuery(this).attr('id').replace('map_canvas_', '')];
+      initLat = postInfo.centerLat != null ? postInfo.centerLat : 31.773827;
+      initLng = postInfo.centerLng != null ? postInfo.centerLng : 130.7518837;
+      initZoom = (postInfo.zoom != null) && postInfo.zoom !== '' ? parseInt(postInfo.zoom, 10) : 10;
+      opt = {
+        zoom: initZoom,
+        center: new google.maps.LatLng(initLat, initLng),
+        mapTypeid: google.maps.MapTypeId.ROADMAP
+      };
+      map = new google.maps.Map(this, opt);
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(initLat, initLng),
+        map: map
+      });
+      map.panTo(new google.maps.LatLng(initLat, initLng));
     });
   };
 
